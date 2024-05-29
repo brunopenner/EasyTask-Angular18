0ffba163f3eac99012d16aa665cfbe679ff274a6
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-05-29 19:37:01 +0800

    #12 & #15 Creating custom component

---
+++ src/app/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-header',
+    // template: "<h1>I'm the header component</h1>",
+    standalone: true,
+    imports: [],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+
+export class HeaderComponent {}
