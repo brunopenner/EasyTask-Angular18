3be24d3748ceb093de745a3310cd2bc63b6f8ac2
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-07 18:54:44 +0800

    #46. Outputting Task Data in the Task Component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -20,15 +20,15 @@ export class tasksComponent {
             summary:
                 'Learn all the basic and advanced features of Angular & how to apply them.',
             dueDate: '2025-12-31',
-            },
-            {
+        },
+        {
             id: 't2',
             userId: 'u3',
             title: 'Build first prototype',
             summary: 'Build a first prototype of the online shop website',
             dueDate: '2024-05-31',
-            },
-            {
+        },
+        {
             id: 't3',
             userId: 'u3',
             title: 'Prepare issue template',
