75068b33780d0a2001183a090b458ff66e012f25
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-23 15:57:22 +0800

    #56. Content Projection with ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,14 +2,15 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { type User } from './user.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from "../shared/card/card.component";
 
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
-  imports:[CommonModule, FormsModule]
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CommonModule, FormsModule, CardComponent]
 })
 
 export class UserComponent {
