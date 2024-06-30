63ec6656b1db9281e0b58fb0d92cfe0c010027f9
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-30 10:49:17 +0800

    final commit

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from "@angular/core";
 import { tasksComponent } from "./tasks.component";
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { SharedModule } from "../shared/shared.module";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -9,7 +12,8 @@ import { NewTaskComponent } from "./new-task/new-task.component";
         TaskComponent, 
         NewTaskComponent
     ],
-    exports: [TaskComponent]
+    exports: [TaskComponent],
+    imports: [SharedModule, CommonModule, FormsModule]
 })
 
 export class TasksModule {}
